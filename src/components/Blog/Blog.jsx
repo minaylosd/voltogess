7bd0f43ccb433939663bfe583904f2dd50208a5f
Dmitry <minaylosd@gmail.com>
2023-06-27 18:48:53 +0400

    blog opacity added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,8 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Blog.module.css"
 import { RevealDiv } from "../RevealDiv/RevealDiv"
 
+
 export const Blog = React.forwardRef((props, ref) => {
+    const [cards, setCards] = useState([
+        {
+            id: 1,
+            category: "Insights",
+            time: "10 min",
+            article: "Academy",
+            date: "05 June 2023",
+            author: "Michael Johnson",
+            description: "With Voltoges‘s commercial storage solutions, commercial and agricultural businesses can optimize their own consumption and thus increase their profitability and independence from the power grid. Voltoges‘s Commercial.",
+            scale: 1,
+            opacity: 1,
+        },
+        {
+            id: 2,
+            scale: 1,
+            opacity: 1,
+            category: "Insights",
+            time: "5 min",
+            article: "Sources",
+            date: "06 June 2023",
+            author: "Elizabeth Ericson",
+            description: "Batteries are specifically designed to meet the requirements of these industries. Thanks to the use of ecological redox flow technology, they are also excellent in terms of safety and durability.",
+            scale: 1,
+            opacity: 1,
+        },
+        {
+            id: 3,
+            category: "Insights",
+            time: "8 min",
+            article: "Applications",
+            date: "22 June 2023",
+            author: "Michael Johnson",
+            description: "In light of advancing political and social efforts to combat climate change, the energy supply is currently in a state of comprehensive change. The generation and use of climate-friendly forms of energy is of crucial importance in this context.",
+            scale: 1,
+            opacity: 1,
+        },
+    ]);
+
+    const handleMouseEnter = (id) => {
+        const updatedCards = cards.map((card) => {
+            if (card.id === id) {
+                return { ...card };
+            } else {
+                return { ...card, scale: 0.95, opacity: 0.55 };
+            }
+        });
+
+        setCards(updatedCards);
+    };
+
+    const handleMouseLeave = () => {
+        const resetCards = cards.map((card) => ({
+            ...card,
+            scale: 1,
+            opacity: 1
+        }));
+
+        setCards(resetCards);
+    };
+    
     return (
         <section className={styles.section} ref={ref}>
             <div className={styles.content}>
@@ -22,54 +83,33 @@ export const Blog = React.forwardRef((props, ref) => {
                     </div>
                 </div>
                 <div className={styles.blog__wrapper}>
-                    <RevealDiv>
-                        <div className={styles.blog__item}>
-                            <div className={styles.info__column}>
-                                <div className={styles.info__wrapper}>
-                                    <div className={styles.category}>Insights</div>
-                                    <div className={styles.time}>10 min</div>
+                    {cards.map((card) => (
+                        <RevealDiv key={card.id}>
+                            <div
+                                key={card.id}
+                                className={styles.blog__item}
+                                onMouseEnter={() => handleMouseEnter(card.id)}
+                                onMouseLeave={handleMouseLeave}
+                                style={{
+                                    transform: `scale(${card.scale})`,
+                                    opacity: card.opacity
+                                }}
+                            >
+                                <div className={styles.info__column}>
+                                    <div className={styles.info__wrapper}>
+                                        <div className={styles.category}>{card.category}</div>
+                                        <div className={styles.time}>{card.time}</div>
+                                    </div>
+                                    <h2 className={styles.article}>{card.article}</h2>
+                                    <div className={styles.info__wrapper}>
+                                        <div className={styles.info}>{card.date}</div>
+                                        <div className={styles.info}>{card.author}</div>
+                                    </div>
                                 </div>
-                                <h2 className={styles.article}>Academy</h2>
-                                <div className={styles.info__wrapper}>
-                                    <div className={styles.info}>05 June 2023</div>
-                                    <div className={styles.info}>Michael Johnson</div>
-                                </div>
+                                <p className={styles.description__column}>{card.description}</p>
                             </div>
-                            <p className={styles.description__column}>With Voltoges&#8216;s commercial storage solutions, commercial and agricultural businesses can optimize their own consumption and thus increase their profitability and independence from the power grid. Voltoges&#8216;s Commercial.</p>
-                        </div>
-                    </RevealDiv>
-                    <RevealDiv>
-                        <div className={styles.blog__item}>
-                            <div className={styles.info__column}>
-                                <div className={styles.info__wrapper}>
-                                    <div className={styles.category}>Insights</div>
-                                    <div className={styles.time}>5 min</div>
-                                </div>
-                                <h2 className={styles.article}>Sources</h2>
-                                <div className={styles.info__wrapper}>
-                                    <div className={styles.info}>06 June 2023</div>
-                                    <div className={styles.info}>Elizabeth Ericson</div>
-                                </div>
-                            </div>
-                            <p className={styles.description__column}>Batteries are specifically designed to meet the requirements of these industries. Thanks to the use of ecological redox flow technology, they are also excellent in terms of safety and durability.</p>
-                        </div>
-                    </RevealDiv>
-                    <RevealDiv>
-                        <div className={styles.blog__item}>
-                            <div className={styles.info__column}>
-                                <div className={styles.info__wrapper}>
-                                    <div className={styles.category}>Insights</div>
-                                    <div className={styles.time}>8 min</div>
-                                </div>
-                                <h2 className={styles.article}>Applications</h2>
-                                <div className={styles.info__wrapper}>
-                                    <div className={styles.info}>22 June 2023</div>
-                                    <div className={styles.info}>Michael Johnson</div>
-                                </div>
-                            </div>
-                            <p className={styles.description__column}>In light of advancing political and social efforts to combat climate change, the energy supply is currently in a state of comprehensive change. The generation and use of climate-friendly forms of energy is of crucial importance in this context.</p>
-                        </div>
-                    </RevealDiv>
+                        </RevealDiv>  
+                    ))}
                 </div>
             </div>
         </section>
