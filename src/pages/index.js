47c1f9e290fd68acefd6ab8b2d4addbb7edb8502
Dmitry <minaylosd@gmail.com>
2023-07-26 12:16:00 +0400

    fix anim

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,66 +18,63 @@ import initSmoothScroll from "@/utils/initSmoothScroll";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
+  const sectionRefs = useRef([]);
 
-    const sectionRefs = useRef([]);
+  useEffect(() => {
+    setTimeout(() => {
+      sectionRefs.current.forEach((sectionRef) => {
+        const gsapItems = Array.from(sectionRef.children).flatMap((child) =>
+          Array.from(child.querySelectorAll(".reveal"))
+        );
 
-    useEffect(() => {
-        setTimeout(() => {
-            sectionRefs.current.forEach((sectionRef) => {
-                const gsapItems = Array.from(sectionRef.children).flatMap((child) =>
-                    Array.from(child.querySelectorAll(".reveal"))
-                );
+        gsap.from(gsapItems, {
+          autoAlpha: 0,
+          y: 150,
+          stagger: 0.05,
+          delay: 0,
+          scrollTrigger: {
+            trigger: sectionRef,
+            start: "top 100%",
+            toggleActions: "play none none none",
+          },
+        });
+      });
+    }, 5000);
+  }, []);
 
-                gsap.from(gsapItems, {
-                    autoAlpha: 0,
-                    y: 150,
-                    stagger: 0.05,
-                    delay: 0,
-                    scrollTrigger: {
-                    trigger: sectionRef,
-                    start: "top 100%",
-                    toggleActions: "play none none none",
-                    },
-                },);
-            });
-        }, 5000)
-    }, []);
-    
-    useEffect(() => {
-        initSmoothScroll(document, 100, 20);
-    }, [])
+  useEffect(() => {
+    initSmoothScroll(document, 100, 20);
+  }, []);
 
-    const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        var tl = gsap.timeline({repeat: 0, repeatDelay: 1});
-        tl.to(".current", {x: "100%", duration: 4, ease: "power.out"});
-        tl.to(".fade", {opacity: 0, duration: 1, onComplete: closePreloader});
-    }, [])
+  useEffect(() => {
+    var tl = gsap.timeline({ repeat: 0, repeatDelay: 1 });
+    tl.to(".current", { x: "100%", duration: 4, ease: "power.out" });
+    tl.to(".fade", { opacity: 0, duration: 1, onComplete: closePreloader });
+  }, []);
 
-    function closePreloader() {
-        setIsLoading(false);
-    }
-
-    
+  function closePreloader() {
+    setIsLoading(false);
+  }
 
   return (
     <div>
-        {isLoading && <Preloader />}
-        <div style={{visibility: isLoading ? "hidden" : "visible" }}>
-            <Header />
-        </div>
-        <div style={{visibility: isLoading ? "hidden" : "visible" }}>
-            <Hero ref={(el) => sectionRefs.current.push(el)} />
-            <Poster ref={(el) => sectionRefs.current.push(el)} />
-            <Company ref={(el) => sectionRefs.current.push(el)} />
-            <TwoTickerSection />
-            <Mission ref={(el) => sectionRefs.current.push(el)} />
-            <Process ref={(el) => sectionRefs.current.push(el)} />
-            <Blog ref={(el) => sectionRefs.current.push(el)} />
-            <Action ref={(el) => sectionRefs.current.push(el)} />
-            <Footer />
-        </div>
+      {isLoading && <Preloader />}
+      <div style={{ visibility: isLoading ? "hidden" : "visible" }}>
+        <Header />
+      </div>
+      <div style={{ visibility: isLoading ? "hidden" : "visible" }}>
+        <Hero ref={(el) => sectionRefs.current.push(el)} />
+        <Poster ref={(el) => sectionRefs.current.push(el)} />
+        <Company ref={(el) => sectionRefs.current.push(el)} />
+        <TwoTickerSection />
+        <Mission ref={(el) => sectionRefs.current.push(el)} />
+        <Process ref={(el) => sectionRefs.current.push(el)} />
+        <Blog ref={(el) => sectionRefs.current.push(el)} />
+        <Action ref={(el) => sectionRefs.current.push(el)} />
+        <Footer />
+      </div>
     </div>
   );
 }
