4ae2db2b9eb09671eb6e7edb8fbba485386f27c2
Dmitry <minaylosd@gmail.com>
2023-07-26 12:07:32 +0400

    add mix blend

--- src/components/Header/Burger.jsx
+++ src/components/Header/Burger.jsx
@@ -1,17 +1,17 @@
-const Burger = ({...props}) => {
+const Burger = ({ ...props }) => {
   return (
     <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={"100%"}
-    height={"100%"}
-    viewBox="0 0 46 46"
-    fill="none"
-    {...props}
-  >
-    <rect width={46} height={46} fill="rgba(255, 255, 255, 0.07)" rx={23} />
-    <path fill="#fff" d="M13.5 18.5h19v1h-19zM13.5 26.5h19v1h-19z" />
-  </svg>
-  )
-}
+      xmlns="http://www.w3.org/2000/svg"
+      width={"100%"}
+      height={"100%"}
+      viewBox="0 0 46 46"
+      fill="none"
+      {...props}
+    >
+      <rect width={46} height={46} fill="#fff" rx={23} />
+      <path fill="#18181A" d="M13.5 18.5h19v1h-19zM13.5 26.5h19v1h-19z" />
+    </svg>
+  );
+};
 
-export default Burger
+export default Burger;
