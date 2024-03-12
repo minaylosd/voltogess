465b970ecfd3594d8abc1e7c067d679b0be07874
minaylosd <minaylosd@gmail.com>
2024-03-12 15:33:09 +0300

    add showmore

--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import styles from "./Mission.module.css";
 import Map from "./Map";
-import Plus from "./Plus";
 import { RevealDiv } from "../RevealDiv/RevealDiv";
+import { ShowMore } from "./ShowMore";
 
 export const Mission = (props) => {
+  const listData = [
+    {
+      heading: "Infrastructure",
+      text: "Voltogess Commercial Batteries enable commercial and agricultural\n businesses to optimize their self-consumption, increasing their\n profitability and independence from the power grid. The generation\n and use of climate-friendly forms of energy is of crucial importance in\n this context. Companies in the commercial and agricultural sectors\n also want to contribute to a carbon-neutral energy supply with\n innovative energy solutions. Ecological energy storage solutions play a\n special role in enabling 100% renewable energy around the clock.",
+    },
+    {
+      heading: "Security management",
+      text: "Voltogess Commercial Batteries enable commercial and agricultural\n businesses to optimize their self-consumption, increasing their\n profitability and independence from the power grid. The generation\n and use of climate-friendly forms of energy is of crucial importance in\n this context. Companies in the commercial and agricultural sectors\n also want to contribute to a carbon-neutral energy supply with\n innovative energy solutions. Ecological energy storage solutions play a\n special role in enabling 100% renewable energy around the clock.",
+    },
+    {
+      heading: "Reduction",
+      text: "Voltogess Commercial Batteries enable commercial and agricultural\n businesses to optimize their self-consumption, increasing their\n profitability and independence from the power grid. The generation\n and use of climate-friendly forms of energy is of crucial importance in\n this context. Companies in the commercial and agricultural sectors\n also want to contribute to a carbon-neutral energy supply with\n innovative energy solutions. Ecological energy storage solutions play a\n special role in enabling 100% renewable energy around the clock.",
+    },
+  ];
   return (
     <section className={`${styles.section} section`}>
       <div className={styles.heading__container}>
@@ -33,32 +47,12 @@ export const Mission = (props) => {
         <div className={styles.mission__list}>
           <RevealDiv>
             <div className={styles.line}></div>
-            <div className={styles.list__item}>
-              <div className={styles.item__name}>Infrastructure</div>
-              <div className={styles.plus__wrapper}>
-                <Plus />
-              </div>
-            </div>
-            <div className={styles.line}></div>
           </RevealDiv>
-          <RevealDiv>
-            <div className={styles.list__item}>
-              <div className={styles.item__name}>Security management</div>
-              <div className={styles.plus__wrapper}>
-                <Plus />
-              </div>
-            </div>
-            <div className={styles.line}></div>
-          </RevealDiv>
-          <RevealDiv>
-            <div className={styles.list__item}>
-              <div className={styles.item__name}>Reduction</div>
-              <div className={styles.plus__wrapper}>
-                <Plus />
-              </div>
-            </div>
-            <div className={styles.line}></div>
-          </RevealDiv>
+          {listData.map((item, index) => (
+            <RevealDiv key={index}>
+              <ShowMore content={item} />
+            </RevealDiv>
+          ))}
         </div>
       </div>
     </section>
---
+++ src/components/Mission/ShowMore.jsx
@@ -0,0 +1,127 @@
+import styles from "./Mission.module.css";
+import { useState, useRef } from "react";
+import { gsap } from "gsap";
+export const ShowMore = ({ ...props }) => {
+  const [isShown, setIsShown] = useState(false);
+  const item = props.content;
+  const textLines = lines(item.text);
+  const showRef = useRef(null),
+    plusRef = useRef(null),
+    textRef = useRef(null);
+
+  function lines(text) {
+    return text.split("\n");
+  }
+
+  function openShowTween() {
+    const elements = textRef.current.querySelectorAll(
+      `[data-animation="lines"]`
+    );
+
+    gsap.to(showRef.current, {
+      height: 323,
+      duration: 0.3,
+    });
+
+    gsap.to(plusRef.current, {
+      rotate: 90,
+      x: -10,
+      y: 10,
+      duration: 0.3,
+    });
+
+    gsap.fromTo(
+      elements,
+      {
+        y: -150,
+        autoAlpha: 0,
+      },
+      {
+        y: 0,
+        autoAlpha: 1,
+        stagger: 0.03,
+        delay: 0.1,
+      }
+    );
+
+    setIsShown(true);
+  }
+
+  function closeShowTween() {
+    const elements = textRef.current.querySelectorAll(
+      `[data-animation="lines"]`
+    );
+
+    gsap.to(showRef.current, {
+      height: 110,
+      duration: 0.3,
+      onComplete: setIsShown(false),
+    });
+
+    gsap.to(plusRef.current, {
+      rotate: 0,
+      x: 0,
+      y: 0,
+      duration: 0.3,
+    });
+
+    gsap.fromTo(
+      elements,
+      {
+        autoAlpha: 1,
+      },
+      {
+        autoAlpha: 0,
+        duration: 0.2,
+      }
+    );
+  }
+
+  function handleShow() {
+    if (isShown == false) {
+      openShowTween();
+    } else {
+      closeShowTween();
+    }
+  }
+
+  return (
+    <>
+      <div ref={showRef} onClick={handleShow} className={styles.list__item}>
+        <div className={styles.list__btn}>
+          <div className={styles.item__heading}>{item.heading}</div>
+          <div className={styles.plus__wrapper}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={"100%"}
+              height={"100%"}
+              fill="none"
+              viewBox="0 0 22 22"
+              {...props}
+            >
+              <path
+                className={styles.plus__rotating}
+                ref={plusRef}
+                fill="#979797"
+                d="M10 0h2v22h-2z"
+              />
+              <path fill="#979797" d="M0 12v-2h22v2z" />
+            </svg>
+          </div>
+        </div>
+        <p ref={textRef} className={styles.item__text}>
+          {textLines.map((line, index) => (
+            <span
+              key={index}
+              className={styles.item__lines}
+              data-animation="lines"
+            >
+              {line}
+            </span>
+          ))}
+        </p>
+      </div>
+      <div className={styles.line}></div>
+    </>
+  );
+};
